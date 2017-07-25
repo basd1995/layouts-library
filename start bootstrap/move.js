a805d637467b0e49f74c8d545cfb953bcb4c11fc
basd1995 <1035102183@qq.com>
2017-07-26 02:11:12 +0800

    add-js

---
+++ start bootstrap/move.js
@@ -0,0 +1,23 @@
+;$(function(){
+    var item1 = $('#item-1');
+    var item2 = $('#item-2');
+    var item3 = $('#item-3');
+    var item4 = $('#item-4');
+    var item5 = $('#item-5');
+    
+    item1.on('click', function(){
+        $('html,body').animate({scrollTop: 700},700)
+    })
+    item2.on('click', function(){
+        $('html,body').animate({scrollTop: 1400},700)
+    })
+    item3.on('click', function(){
+        $('html,body').animate({scrollTop: 2600},700)
+    })
+    item4.on('click', function(){
+        $('html,body').animate({scrollTop: 4300},700)
+    })
+    item5.on('click', function(){
+        $('html,body').animate({scrollTop: 5280},700)
+    })
+})
